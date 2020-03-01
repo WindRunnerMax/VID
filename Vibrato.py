77535b074dd4f3112501a99c8c7618f74eecbd19
Czy <651525974@qq.com>
2020-03-01 13:29:47 +0800

    Fix

--- Vibrato.py
+++ Vibrato.py
@@ -13,10 +13,10 @@ from PyQt5.QtWidgets import *
 from PyQt5 import QtWidgets,QtGui
 from form import Ui_Form
 
-VUrl = "http://v.douyin.com/YmuKu5/"
+VUrl = "https://v.douyin.com/gWc3BC"
 
 VHEADERS = {
-'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36 OPR/58.0.3135.65',
+'User-Agent':'Mozilla/5.0 (Linux; U; Android 5.1.1; zh-cn; MI 4S Build/LMY47V) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/53.0.2785.146 Mobile Safari/537.36 XiaoMi/MiuiBrowser/9.1.3',
 "accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
 "accept-encoding":"gzip, deflate, sdch, br",
 "accept-language":"en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4",
@@ -38,12 +38,11 @@ class Vibrato(QDialog,Ui_Form):
         req = session.get(self.url , timeout = 5 , headers = self.HEADERS)
         req.encoding = 'utf-8'
         data = req.text
-        cover = data.split("cover: \"")[1].split("\"")[0]
-        playAddr = data.split("playAddr: \"")[1].split("\",")[0]
-        videoId = playAddr.split("video_id=")[1].split("&")[0]
+        matchData = re.findall( r'<video id="theVideo" class="video-player" src="([\S]*?)" preload="auto"',data)
+        playAddr = matchData[0].replace("/playwm/","/play/")
+        videoId = data.split("itemId: \"")[1].split("\",")[0]
         videoAddr = playAddr.replace("/playwm/","/play/");
         return {
-            "cover": cover,
             "playAddr": playAddr,
             "addr": videoAddr,
             "id": videoId
@@ -81,11 +80,8 @@ class Vibrato(QDialog,Ui_Form):
                 self.label.setText(str(e))
         
     def test(self):
-        try:
-            info,session = self.GetRealUrl()
-            self.Download(info,session)
-        except Exception as e:
-            print("内部错误")
+        info,session = self.GetRealUrl()
+        self.Download(info,session)
 
 
 if __name__ == '__main__':
