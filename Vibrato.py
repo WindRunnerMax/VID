a98d64a0d4db3659ce71f48a882557c387f1a952
Czy <651525974@qq.com>
2021-07-07 16:04:12 +0800

    path revise

--- Vibrato.py
+++ Vibrato.py
@@ -9,7 +9,7 @@ HEADERS = {
     "accept-encoding":"deflate",
     "accept-language":"en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4",
     "cache-control":"no-cache",
-    'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.92 Safari/537.36',
+    "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.92 Safari/537.36",
 }
 
 H0 = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.92 Safari/537.36"
@@ -26,19 +26,19 @@ class Vibrato():
     def __get_real_url(self):
         session = requests.Session()
         req = session.get(self.url , timeout = 5 , headers = self.headers)
-        vid = req.url.split("/")[5]
+        vid = req.url.split("/")[4].split("?")[0]
         videoInfo = session.get("https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids=" + vid,
             timeout = 5 , headers = self.headers)
         playAddr = re.findall(r'https://aweme.snssdk.com/aweme[\S]*?"',videoInfo.text)[0][:-1]
         parsedAddr = playAddr.replace("/playwm/","/play/")
-        return vid,parsedAddr,session
+        return vid, parsedAddr, session
 
     def __download(self, vid, info, session):
-        self.headers['user-agent'] = H1
+        self.headers["user-agent"] = H1
         videoBin = session.get(info,timeout = 5, headers = self.headers );
-        with open('%s.mp4' % (vid),'wb') as fb:
+        with open("%s.mp4" % (vid),"wb") as fb:
             fb.write(videoBin.content)
-        self.headers['user-agent'] = H0
+        self.headers["user-agent"] = H0
         return "下载完成"
 
     def run(self, url):
